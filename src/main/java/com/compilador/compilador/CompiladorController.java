92bfd1b6a9d8aad7fd161e62ef72b518518bc0ba
Karim Rios <gadkarim12@gmail.com>
2026-04-06 20:43:01 -0600

    Se agrego el boton para agregar archivos

--- src/main/java/com/compilador/compilador/CompiladorController.java
+++ src/main/java/com/compilador/compilador/CompiladorController.java
@@ -2,6 +2,9 @@ package com.compilador.compilador;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import java.io.File;
 
 public class CompiladorController {
 
@@ -10,7 +13,15 @@ public class CompiladorController {
 
     @FXML
     private void abrirArchivo() {
-
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Abrir archivo KScript");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Codigo de KScript", "*.txt")
+        );
+        File archivo = fileChooser.showOpenDialog(new Stage());
+        if (archivo != null) {
+            panelErrores.setText("Archivo cargado: " + archivo.getName());
+        }
     }
 
     @FXML
